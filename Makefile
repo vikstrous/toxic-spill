43cce57ba6f668e9a02a432cc8ab34f92edfeeaf
Viktor Stanchev <me@viktorstanchev.com>
2015-06-25 14:36:11 -0700

    fix makefile and add ability to check active connections

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 .PHONY: all
 
 all: main/app.go
-	docker run -v $(PWD)/Godeps/_workspace:/Godeps -v $(PWD):/usr/src/github.com/vikstrous/toxic-spill -e "GOPATH=/Godeps:/usr/src" -w /usr/src/github.com/vikstrous/toxic-spill golang:1.4.2 go build -o toxic-spill ./main
+	docker run -v $$(pwd)/Godeps/_workspace:/Godeps -v $$(pwd):/usr/src/github.com/vikstrous/toxic-spill -e "GOPATH=/Godeps:/usr/src" -w /usr/src/github.com/vikstrous/toxic-spill golang:1.4.2 go build -o toxic-spill ./main
--- main/app.go
+++ main/app.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"regexp"
 	"strings"
 
 	toxiproxy "github.com/Shopify/toxiproxy/client"
@@ -15,6 +17,31 @@ var tp *toxiproxy.Client
 
 func addProxyHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi")
+}
+
+func getActiveConns() [][]string {
+	iftop_out, err := exec.Command("iftop", "-n", "-i", "docker0", "-P", "-N", "-t", "-s", "1").Output()
+	if err != nil {
+		log.Printf("Failed to execute iftop")
+		return nil
+	}
+
+	results := [][]string{}
+
+	re := regexp.MustCompile("(\\d+(\\.\\d+){3}):(\\d+)")
+	conns := strings.Split(fmt.Sprintf("%s", iftop_out), "\n--------------------------------------------------------------------------------------------\n")[1]
+	conns_arr := strings.Split(conns, "\n")
+	for i := 0; i < len(conns_arr); i+=2 {
+		ip_port_dst := re.FindStringSubmatch(conns_arr[i])
+		ip_dst := ip_port_dst[1]
+		port_dst := ip_port_dst[3]
+		ip_port_src := re.FindStringSubmatch(conns_arr[i+1])
+		ip_src := ip_port_src[1]
+		//port_src := ip_port_src[3]
+		//log.Printf("%s connecting to %s on port %s", ip_src, ip_dst, port_dst)
+		results = append(results, []string{ip_src, ip_dst, port_dst})
+	}
+	return results
 }
 
 func main() {
@@ -35,6 +62,8 @@ func main() {
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(fs)
 	r.HandleFunc("/proxy", addProxyHandler).Methods("GET")
+
+	log.Println(getActiveConns())
 
 	log.Println("Listening on 3000...")
 	http.ListenAndServe(":3000", r)
